f4611f2241452922bd3d3befb73de7ea9958d237
Fujisan-3776 <axela20120727@gmail.com>
2020-10-24 15:23:30 +0900

    itemsテーブルのカラム名とActiveHashのモデル名の不一致を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre_category
-  belongs_to_active_hash :genre_delivery_charge
-  belongs_to_active_hash :genre_item_condition
-  belongs_to_active_hash :genre_prefecture
-  belongs_to_active_hash :genre_shipping_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_days
 
   belongs_to :user
   has_one :ordr
